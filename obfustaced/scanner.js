8fd16e231d2245595cae60d6313e77091fee2012
dinimm45 <73088172+dinimm45@users.noreply.github.com>
2023-06-11 12:54:32 +0200

    Update scanner.js

--- obfustaced/scanner.js
+++ obfustaced/scanner.js
@@ -1,187 +1,211 @@
-function _0x4505(_0x3edf3d, _0x214379) {
-  const _0x5b9a1c = _0x5b9a();
+function _0x3f60(_0x5b8f1f, _0xb6d31f) {
+  const _0x3314f5 = _0x3314();
   return (
-    (_0x4505 = function (_0x4505cf, _0x1503fc) {
-      _0x4505cf = _0x4505cf - 0x19d;
-      let _0x228101 = _0x5b9a1c[_0x4505cf];
-      return _0x228101;
+    (_0x3f60 = function (_0x3f6076, _0x213c95) {
+      _0x3f6076 = _0x3f6076 - 0xd3;
+      let _0x147340 = _0x3314f5[_0x3f6076];
+      return _0x147340;
     }),
-    _0x4505(_0x3edf3d, _0x214379)
+    _0x3f60(_0x5b8f1f, _0xb6d31f)
   );
 }
-(function (_0x424cc4, _0x494c5b) {
-  const _0x2f40b1 = _0x4505,
-    _0x1046fa = _0x424cc4();
+function _0x3314() {
+  const _0x5d9086 = [
+    "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js",
+    "forEach",
+    "getContext",
+    "innerText",
+    "getUserMedia",
+    "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js",
+    "getElementById",
+    "display",
+    "AIzaSyD-e_171y9kifCgevAMRElGKnHghxzdM7A",
+    "14105808IrkIBl",
+    "mediaDevices",
+    "qr-canvas",
+    "G-J0DC7YW8J4",
+    "video",
+    "playsinline",
+    "width",
+    "4SpICBW",
+    "decode",
+    "eventsdb-b6aaf.firebaseapp.com",
+    "height",
+    "refresh",
+    "play",
+    "995789907234",
+    "282689qVAWrt",
+    "7YmlyIw",
+    "click",
+    "1:995789907234:web:54edcc689b1c894594717b",
+    "code",
+    "1102780sDETiP",
+    "2077020OJuqWY",
+    "490wUHBGu",
+    "12020848XNynkA",
+    "eventsdb-b6aaf",
+    "then",
+    "nonevalid",
+    "eventsdb-b6aaf.appspot.com",
+    "addEventListener",
+    "3gdDPlM",
+    "log",
+    "reload",
+    "callback",
+    "block",
+    "2584884uGuEOa",
+    "videoWidth",
+    "data",
+    "srcObject",
+    "1439095KYPXPg",
+    "hidden",
+  ];
+  _0x3314 = function () {
+    return _0x5d9086;
+  };
+  return _0x3314();
+}
+(function (_0x5e5b5e, _0x3375a0) {
+  const _0x3e7cd9 = _0x3f60,
+    _0x3673b3 = _0x5e5b5e();
   while (!![]) {
     try {
-      const _0x1137e0 =
-        (-parseInt(_0x2f40b1(0x19e)) / 0x1) *
-          (-parseInt(_0x2f40b1(0x1c0)) / 0x2) +
-        (parseInt(_0x2f40b1(0x1b0)) / 0x3) *
-          (-parseInt(_0x2f40b1(0x1a4)) / 0x4) +
-        (parseInt(_0x2f40b1(0x1b7)) / 0x5) *
-          (-parseInt(_0x2f40b1(0x1a9)) / 0x6) +
-        parseInt(_0x2f40b1(0x1c3)) / 0x7 +
-        parseInt(_0x2f40b1(0x1ba)) / 0x8 +
-        (parseInt(_0x2f40b1(0x1bb)) / 0x9) *
-          (-parseInt(_0x2f40b1(0x1af)) / 0xa) +
-        (parseInt(_0x2f40b1(0x19d)) / 0xb) * (parseInt(_0x2f40b1(0x1b1)) / 0xc);
-      if (_0x1137e0 === _0x494c5b) break;
-      else _0x1046fa["push"](_0x1046fa["shift"]());
-    } catch (_0x58d2c6) {
-      _0x1046fa["push"](_0x1046fa["shift"]());
+      const _0x10008d =
+        (-parseInt(_0x3e7cd9(0xf8)) / 0x1) * (parseInt(_0x3e7cd9(0xef)) / 0x2) +
+        parseInt(_0x3e7cd9(0xf0)) / 0x3 +
+        (parseInt(_0x3e7cd9(0xe3)) / 0x4) *
+          (-parseInt(_0x3e7cd9(0x101)) / 0x5) +
+        parseInt(_0x3e7cd9(0xfd)) / 0x6 +
+        (-parseInt(_0x3e7cd9(0xeb)) / 0x7) *
+          (-parseInt(_0x3e7cd9(0xf2)) / 0x8) +
+        parseInt(_0x3e7cd9(0xdc)) / 0x9 +
+        (-parseInt(_0x3e7cd9(0xf1)) / 0xa) * (parseInt(_0x3e7cd9(0xea)) / 0xb);
+      if (_0x10008d === _0x3375a0) break;
+      else _0x3673b3["push"](_0x3673b3["shift"]());
+    } catch (_0xef1868) {
+      _0x3673b3["push"](_0x3673b3["shift"]());
     }
   }
-})(_0x5b9a, 0xcf841),
+})(_0x3314, 0xf2258),
   (async () => {
-    const _0x4ebd7e = _0x4505,
-      { initializeApp: _0x4cc7a2 } = await import(_0x4ebd7e(0x1a8)),
-      { getAnalytics: _0xe83bb7 } = await import(
-        "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js"
+    const _0x3c7df4 = _0x3f60,
+      { initializeApp: _0x591381 } = await import(
+        "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
       ),
-      { getAuth: _0x7dce55 } = await import(_0x4ebd7e(0x1c2)),
+      { getAnalytics: _0x568c99 } = await import(_0x3c7df4(0xd8)),
+      { getAuth: _0x45e98c } = await import(_0x3c7df4(0xd3)),
       {
-        getFirestore: _0x373a01,
-        doc: _0x31ea26,
-        getDoc: _0x77fae3,
-        setDoc: _0x179be2,
-        collection: _0x22c1d6,
-        addDoc: _0x4880fa,
-        updateDoc: _0xeef3b9,
-        deleteDoc: _0x5d419e,
-        deleteField: _0x90acf5,
-        arrayRemove: _0x288af2,
-        query: _0x57146e,
-        where: _0x1b06cc,
-        getDocs: _0x24d8a6,
-      } = await import(_0x4ebd7e(0x1bd)),
-      _0x509b1f = {
-        apiKey: _0x4ebd7e(0x1aa),
-        authDomain: _0x4ebd7e(0x1b9),
-        projectId: "eventsdb-b6aaf",
-        storageBucket: _0x4ebd7e(0x1c5),
-        messagingSenderId: _0x4ebd7e(0x1c1),
-        appId: _0x4ebd7e(0x1be),
-        measurementId: _0x4ebd7e(0x1a6),
+        getFirestore: _0xc52bfe,
+        doc: _0x3867f4,
+        getDoc: _0x12e086,
+        setDoc: _0x54ac72,
+        collection: _0xc3b6f8,
+        addDoc: _0x40e5e1,
+        updateDoc: _0x3c4f46,
+        deleteDoc: _0x310a28,
+        deleteField: _0x1cf6f4,
+        arrayRemove: _0x5f3e44,
+        query: _0x546fd5,
+        where: _0x366027,
+        getDocs: _0x2bb4e7,
+      } = await import(
+        "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js"
+      ),
+      _0x53ce0b = {
+        apiKey: _0x3c7df4(0xdb),
+        authDomain: _0x3c7df4(0xe5),
+        projectId: _0x3c7df4(0xf3),
+        storageBucket: _0x3c7df4(0xf6),
+        messagingSenderId: _0x3c7df4(0xe9),
+        appId: _0x3c7df4(0xed),
+        measurementId: _0x3c7df4(0xdf),
       },
-      _0x43115a = _0x4cc7a2(_0x509b1f),
-      _0x53da42 = _0xe83bb7(_0x43115a),
-      _0x7977e = _0x373a01(),
-      _0x206a55 = document[_0x4ebd7e(0x1ad)]("video"),
-      _0x1395b8 = document[_0x4ebd7e(0x1ad)](_0x4ebd7e(0x1a2)),
-      _0x25fa1b = document[_0x4ebd7e(0x1ad)]("outputData"),
-      _0x1a99e7 = document[_0x4ebd7e(0x1ad)]("valid"),
-      _0x4dff5d = document[_0x4ebd7e(0x1ad)](_0x4ebd7e(0x1b2)),
-      _0x14754b = document[_0x4ebd7e(0x1ad)]("refresh"),
-      _0x3c703d = { video: { facingMode: _0x4ebd7e(0x1b8) } };
-    let _0x287869 = [];
-    const _0x563681 = await _0x24d8a6(_0x22c1d6(_0x7977e, "codes"));
-    _0x563681["forEach"]((_0xfea183) => {
-      const _0x58379e = _0x4ebd7e;
-      _0x287869[_0x58379e(0x1a3)]({
-        id: _0xfea183["id"],
-        code: _0xfea183[_0x58379e(0x1ae)]()[_0x58379e(0x1ac)],
-      });
-    });
-    const _0x1d40a5 = await navigator[_0x4ebd7e(0x1b6)][_0x4ebd7e(0x1ab)](
-      _0x3c703d
-    );
-    (_0x206a55[_0x4ebd7e(0x19f)] = _0x1d40a5), _0x206a55["play"]();
-    const _0x29000d = new BarcodeDetector(),
-      _0x31f250 = async () => {
-        const _0x302b46 = _0x4ebd7e;
-        try {
-          const _0x85e92a = await _0x29000d[_0x302b46(0x1a5)](_0x206a55);
-          for (const _0x7ec88 of _0x85e92a) {
-            const _0xb56d00 = _0x7ec88[_0x302b46(0x1bc)],
-              _0x30df18 = _0x287869[_0x302b46(0x1c4)](
-                (_0x51cfb1) => _0x51cfb1[_0x302b46(0x1ac)] === _0xb56d00
-              );
-            if (_0x30df18) {
-              (_0x25fa1b["innerText"] = _0xb56d00),
-                (_0x1a99e7[_0x302b46(0x1b3)][_0x302b46(0x1b5)] =
-                  _0x302b46(0x1c6)),
-                (_0x14754b[_0x302b46(0x1b3)][_0x302b46(0x1b5)] =
-                  _0x302b46(0x1c6)),
-                _0x14754b["addEventListener"]("click", () => {
-                  const _0x398138 = _0x302b46;
-                  location[_0x398138(0x1a0)]();
+      _0x5c1af1 = _0x591381(_0x53ce0b),
+      _0x1eacd0 = _0x568c99(_0x5c1af1),
+      _0x347be0 = _0xc52bfe(),
+      _0x797104 = window["qrcode"],
+      _0x118e37 = document[_0x3c7df4(0xd9)](_0x3c7df4(0xe0)),
+      _0x2af6cb = document[_0x3c7df4(0xd9)](_0x3c7df4(0xde)),
+      _0x5d58cf = _0x2af6cb[_0x3c7df4(0xd5)]("2d"),
+      _0x550f16 = document[_0x3c7df4(0xd9)]("qr-result"),
+      _0x49be3b = document[_0x3c7df4(0xd9)]("outputData"),
+      _0xc7b7df = document[_0x3c7df4(0xd9)]("valid"),
+      _0x41cb13 = document[_0x3c7df4(0xd9)](_0x3c7df4(0xf5)),
+      _0x20cf59 = document[_0x3c7df4(0xd9)](_0x3c7df4(0xe7)),
+      _0x1519f9 = _0x2bb4e7(_0xc3b6f8(_0x347be0, "codes"))[_0x3c7df4(0xf4)](
+        (_0x2cb7c7) => {
+          const _0x45ddcb = _0x3c7df4;
+          let _0x192531 = ![];
+          (_0x797104[_0x45ddcb(0xfb)] = (_0x461177) => {
+            const _0x4764f8 = _0x45ddcb;
+            let _0xc3c4d0 = ![],
+              _0x4c5efa = "";
+            _0x2cb7c7[_0x4764f8(0xd4)]((_0x65f416) => {
+              const _0x4f185e = _0x4764f8;
+              _0x65f416[_0x4f185e(0xff)]()[_0x4f185e(0xee)] === _0x461177 &&
+                ((_0xc3c4d0 = !![]), (_0x4c5efa = _0x65f416["id"]));
+            });
+            if (_0xc3c4d0) {
+              (_0x49be3b[_0x4764f8(0xd6)] = _0x461177),
+                (_0xc7b7df["style"]["display"] = "block"),
+                (_0x20cf59["style"][_0x4764f8(0xda)] = "block"),
+                _0x20cf59[_0x4764f8(0xf7)](_0x4764f8(0xec), () => {
+                  const _0x354d06 = _0x4764f8;
+                  location[_0x354d06(0xfa)]();
                 });
-              const _0x165775 = async () => {
-                const _0x93e02c = _0x302b46;
+              const _0x193f03 = async () => {
+                const _0x3d30be = _0x4764f8;
                 try {
-                  await _0x5d419e(
-                    _0x31ea26(_0x7977e, "codes", _0x30df18["id"])
-                  );
-                } catch (_0x346e09) {
-                  console[_0x93e02c(0x1a1)](_0x346e09);
+                  await _0x310a28(_0x3867f4(_0x347be0, "codes", _0x4c5efa));
+                } catch (_0x4614d8) {
+                  console[_0x3d30be(0xf9)](_0x4614d8);
                 }
               };
-              _0x165775(), (_0x1395b8[_0x302b46(0x1a7)] = ![]);
+              _0x193f03(),
+                (_0x550f16["hidden"] = ![]),
+                (_0x2af6cb[_0x4764f8(0x102)] = ![]);
             } else
-              (_0x4dff5d["style"][_0x302b46(0x1b5)] = _0x302b46(0x1c6)),
-                (_0x14754b[_0x302b46(0x1b3)][_0x302b46(0x1b5)] =
-                  _0x302b46(0x1c6)),
-                _0x14754b[_0x302b46(0x1b4)](_0x302b46(0x1bf), () => {
-                  const _0x455ec5 = _0x302b46;
-                  location[_0x455ec5(0x1a0)]();
+              (_0x41cb13["style"][_0x4764f8(0xda)] = _0x4764f8(0xfc)),
+                (_0x20cf59["style"][_0x4764f8(0xda)] = _0x4764f8(0xfc)),
+                _0x20cf59[_0x4764f8(0xf7)](_0x4764f8(0xec), () => {
+                  const _0x1db859 = _0x4764f8;
+                  location[_0x1db859(0xfa)]();
                 });
+          }),
+            navigator[_0x45ddcb(0xdd)]
+              [_0x45ddcb(0xd7)]({ video: { facingMode: "environment" } })
+              [_0x45ddcb(0xf4)](function (_0x289d07) {
+                const _0x2a097a = _0x45ddcb;
+                (_0x192531 = !![]),
+                  (_0x550f16["hidden"] = !![]),
+                  (_0x2af6cb[_0x2a097a(0x102)] = ![]),
+                  _0x118e37["setAttribute"](_0x2a097a(0xe1), !![]),
+                  (_0x118e37[_0x2a097a(0x100)] = _0x289d07),
+                  _0x118e37[_0x2a097a(0xe8)](),
+                  _0x3eba21(),
+                  _0x21e669();
+              });
+          function _0x3eba21() {
+            const _0x558e33 = _0x45ddcb;
+            (_0x2af6cb[_0x558e33(0xe6)] = _0x118e37["videoHeight"]),
+              (_0x2af6cb["width"] = _0x118e37[_0x558e33(0xfe)]),
+              _0x5d58cf["drawImage"](
+                _0x118e37,
+                0x2,
+                0x2,
+                _0x2af6cb[_0x558e33(0xe2)],
+                _0x2af6cb[_0x558e33(0xe6)]
+              ),
+              _0x192531 && requestAnimationFrame(_0x3eba21);
           }
-          requestAnimationFrame(_0x31f250);
-        } catch (_0x142129) {
-          console[_0x302b46(0x1a1)](_0x142129),
-            requestAnimationFrame(_0x31f250);
+          function _0x21e669() {
+            const _0x65ee10 = _0x45ddcb;
+            try {
+              _0x797104[_0x65ee10(0xe4)]();
+            } catch (_0x1e2602) {
+              setTimeout(_0x21e669, 0x64);
+            }
+          }
         }
-      };
-    requestAnimationFrame(_0x31f250);
+      );
   })();
-function _0x5b9a() {
-  const _0x3cff5c = [
-    "6xxonAk",
-    "995789907234",
-    "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js",
-    "2591785hKFWoK",
-    "find",
-    "eventsdb-b6aaf.appspot.com",
-    "block",
-    "33GVrksX",
-    "43ombGxA",
-    "srcObject",
-    "reload",
-    "log",
-    "qr-result",
-    "push",
-    "261988XADGvS",
-    "detect",
-    "G-J0DC7YW8J4",
-    "hidden",
-    "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js",
-    "522tVYThM",
-    "AIzaSyD-e_171y9kifCgevAMRElGKnHghxzdM7A",
-    "getUserMedia",
-    "code",
-    "getElementById",
-    "data",
-    "50WvfMnJ",
-    "39ddfvxB",
-    "8274828LdBOWa",
-    "nonevalid",
-    "style",
-    "addEventListener",
-    "display",
-    "mediaDevices",
-    "21295jmZnkW",
-    "environment",
-    "eventsdb-b6aaf.firebaseapp.com",
-    "1788624aRmwIh",
-    "1063242uenaPk",
-    "rawValue",
-    "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js",
-    "1:995789907234:web:54edcc689b1c894594717b",
-    "click",
-  ];
-  _0x5b9a = function () {
-    return _0x3cff5c;
-  };
-  return _0x5b9a();
-}
