8e9099da1153c10793c167eddd09f7cbc1fa962b
dinimm45 <73088172+dinimm45@users.noreply.github.com>
2023-07-24 16:02:19 +0300

    Add files via upload

---
+++ obfustaced/managementemp.js
@@ -0,0 +1,71 @@
+function _0x4623(_0x2cc8ae, _0x53c160) {
+  var _0x56a3df = _0x56a3();
+  return (
+    (_0x4623 = function (_0x4623bc, _0x54b53a) {
+      _0x4623bc = _0x4623bc - 0xd3;
+      var _0x3dd03e = _0x56a3df[_0x4623bc];
+      return _0x3dd03e;
+    }),
+    _0x4623(_0x2cc8ae, _0x53c160)
+  );
+}
+function _0x56a3() {
+  var _0x4eab55 = [
+    "displayempl.html",
+    "addEventListener",
+    "1098680UTcbNT",
+    "26sPZSMj",
+    "759BPoqHW",
+    "checkin",
+    "356355VJjdan",
+    "29448UjFUyR",
+    "16TnmfZj",
+    "237903vjdMTU",
+    "href",
+    "1036280JEQkTr",
+    "28DBkxwv",
+    "44920shXSZN",
+    "click",
+    "location",
+    "20572DEdPmb",
+  ];
+  _0x56a3 = function () {
+    return _0x4eab55;
+  };
+  return _0x56a3();
+}
+var _0x2dbdcb = _0x4623;
+(function (_0x24d68a, _0x384d79) {
+  var _0x3cd1b5 = _0x4623,
+    _0x28926 = _0x24d68a();
+  while (!![]) {
+    try {
+      var _0xc4b150 =
+        (parseInt(_0x3cd1b5(0xd8)) / 0x1) * (parseInt(_0x3cd1b5(0xdc)) / 0x2) +
+        (parseInt(_0x3cd1b5(0xe2)) / 0x3) * (parseInt(_0x3cd1b5(0xe1)) / 0x4) +
+        -parseInt(_0x3cd1b5(0xdb)) / 0x5 +
+        (parseInt(_0x3cd1b5(0xe0)) / 0x6) * (parseInt(_0x3cd1b5(0xd4)) / 0x7) +
+        parseInt(_0x3cd1b5(0xd3)) / 0x8 +
+        -parseInt(_0x3cd1b5(0xdf)) / 0x9 +
+        (parseInt(_0x3cd1b5(0xd5)) / 0xa) * (-parseInt(_0x3cd1b5(0xdd)) / 0xb);
+      if (_0xc4b150 === _0x384d79) break;
+      else _0x28926["push"](_0x28926["shift"]());
+    } catch (_0x3d843f) {
+      _0x28926["push"](_0x28926["shift"]());
+    }
+  }
+})(_0x56a3, 0x282b0),
+  document["getElementById"]("displayhr")[_0x2dbdcb(0xda)](
+    "click",
+    function () {
+      var _0xa0bc23 = _0x2dbdcb;
+      window[_0xa0bc23(0xd7)][_0xa0bc23(0xe3)] = _0xa0bc23(0xd9);
+    }
+  ),
+  document["getElementById"](_0x2dbdcb(0xde))[_0x2dbdcb(0xda)](
+    _0x2dbdcb(0xd6),
+    function () {
+      var _0x27bede = _0x2dbdcb;
+      window[_0x27bede(0xd7)]["href"] = "scanempl.html";
+    }
+  );
